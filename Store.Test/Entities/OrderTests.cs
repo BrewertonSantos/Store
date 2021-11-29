f8bf5c6159a23b0d8ab154fe6aa4d2cae7813925
Brewerton Santos <brewerton.santos@gmail.com>
2021-11-29 00:10:51 -0300

    Adicionadas ações do método.

--- Store.Test/Entities/OrderTests.cs
+++ Store.Test/Entities/OrderTests.cs
@@ -1,4 +1,7 @@
-﻿namespace Store.Test.Entities;
+﻿using System;
+using Store.Domain.Entities;
+
+namespace Store.Test.Entities;
 
 [TestClass]
 public class OrderTests
@@ -7,7 +10,10 @@ public class OrderTests
     [TestCategory("Domain")]
     public void IsValidNewOrder_Generate8DigitsNumber()
     {
-            Assert.Fail();
+        Customer customer = new("Brewerton Santos","mail@mail.com");
+        Discount discount = new(10, DateTime.Now.AddDays(10));
+        Order order = new(customer, 10, discount);
+        Assert.AreEqual(8, order.Number.Length);
     }
 
     [TestMethod]
@@ -37,49 +43,49 @@ public class OrderTests
     {
         Assert.Fail();
     }
-    
+
     [TestMethod]
     [TestCategory("Domain")]
     public void IsValidNewOrderWithAmountEqualsOrMinorZero_DontAdd()
     {
         Assert.Fail();
     }
-    
+
     [TestMethod]
     [TestCategory("Domain")]
     public void IsValidNewOrder_TotalEquals50()
     {
         Assert.Fail();
     }
-    
+
     [TestMethod]
     [TestCategory("Domain")]
     public void IsExpiredDiscount_OrderValueEquals60()
     {
         Assert.Fail();
     }
-    
+
     [TestMethod]
     [TestCategory("Domain")]
     public void IsInvalidRequest_OrderValueEquals60()
     {
         Assert.Fail();
     }
-    
+
     [TestMethod]
     [TestCategory("Domain")]
     public void IsDiscount10_OrderValueEquals50()
     {
         Assert.Fail();
     }
-    
+
     [TestMethod]
     [TestCategory("Domain")]
     public void IsDeliveryFee10_OrderValueEquals60()
     {
         Assert.Fail();
     }
-    
+
     [TestMethod]
     [TestCategory("Domain")]
     public void IsOrderWithoutClient_Invalid()
