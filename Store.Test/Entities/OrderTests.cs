5e681b94d074972cde6974dfa99427cfc25c2da8
Brewerton Santos <brewerton.santos@gmail.com>
2021-11-28 23:36:00 -0300

    Adicionado projeto de teste e métodos principais.

---
+++ Store.Test/Entities/OrderTests.cs
@@ -0,0 +1,89 @@
+﻿namespace Store.Test.Entities;
+
+[TestClass]
+public class OrderTests
+{
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void IsValidNewOrder_Generate8DigitsNumber()
+    {
+            Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void IsValidNewOrder_WaitingPayment()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void IsValidNewOrder_WaitingDelivery()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void IsValidNewOrder_Canceled()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void IsValidNewOrderWithoutProduct_DontAdd()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void IsValidNewOrderWithAmountEqualsOrMinorZero_DontAdd()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void IsValidNewOrder_TotalEquals50()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void IsExpiredDiscount_OrderValueEquals60()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void IsInvalidRequest_OrderValueEquals60()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void IsDiscount10_OrderValueEquals50()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void IsDeliveryFee10_OrderValueEquals60()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void IsOrderWithoutClient_Invalid()
+    {
+        Assert.Fail();
+    }
+}
